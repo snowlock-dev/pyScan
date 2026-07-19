563fa070a297ac7e88c0628ac0b750387111d8b4
AJ (snowlock-dev) <305949454+snowlock-dev@users.noreply.github.com>
2026-07-19 21:57:04 +0530

    feat: added input from cli for host_name and port range

--- pyscan.py
+++ pyscan.py
@@ -3,10 +3,17 @@
 import socket
 import sys
 
-target_host = "127.0.0.1"
+target_host = input("Enter Host IP (default: 127.0.0.1):").strip()
 
-start_port = 1
-end_port = 1024
+if not target_host:
+    target_host = "127.0.0.1"
+
+start_in = input("Enter starting port (default: 1):").strip()
+end_in = input("Enter starting port (default: 1024):").strip()
+
+start_port = int(start_in) if start_in else 1
+end_port   = int(end_in) if end_in else 1024
+
 
 print(f"[*] Started TCP scan for host: {target_host}")
 print(f"[*] Scanning ports {start_port} to {end_port}...")
@@ -15,16 +22,12 @@ print(f"[*] Scanning ports {start_port} to {end_port}...")
 try:
     for port in range(start_port, end_port + 1):
         
-        soc = socket.socket(socket.AF_INET,         socket.SOCK_STREAM)
-        
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         soc.settimeout(1)
-        
-        result = soc.connect_ex((target_host,       port))
+        result = soc.connect_ex((target_host, port))
         
         if result == 0:
             print(f"Port {port} is open.")
-        else:
-            print(f"Port {port} is closed.")
         
         soc.close()
 
