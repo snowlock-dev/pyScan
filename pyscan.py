933c6223b5a26c2c674e21c734f2b20ad849a1bb
AJ (snowlock-dev) <305949454+snowlock-dev@users.noreply.github.com>
2026-07-24 12:03:34 +0530

    fix: fixed start_port > end_port edge case

--- pyscan.py
+++ pyscan.py
@@ -5,7 +5,6 @@ import sys
 from concurrent.futures import ThreadPoolExecutor
 
 
-
 def scan_port(target_host, port, timeout=1.0):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
@@ -23,6 +22,11 @@ def scan_port(target_host, port, timeout=1.0):
 
 
 def run_scan(target_host="127.0.0.1", start_port=1, end_port=1024, max_workers=100):
+    start_port, end_port = max(1, start_port), min(65535, end_port)
+    if start_port > end_port:
+        print(f"[x] Port range invalid!")
+        return []
+    
     ports_to_scan = range (start_port, end_port + 1)
     open_ports = []
 
